4b8b5b1ffb38811ba760fd798fbd9b1294473ee6
Cindy Chiao <cindy@carbonplan.org>
2022-01-20 22:19:18 +0000

    tweaks

--- srcnn/srcnn.py
+++ srcnn/srcnn.py
@@ -8,11 +8,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 
-import utils
+from . import utils
 
 def _maybe_pad_x(x, padding, is_training):
     if padding == 0:
-       x_pad = x
+        x_pad = x
     elif padding > 0:
         x_pad = tf.cond(pred=is_training, true_fn=lambda: x,
                         false_fn=lambda: utils.replicate_padding(x, padding))
@@ -45,24 +45,20 @@ class SRCNN:
             self.device = '/cpu:0'
 
         self.global_step = tf.Variable(0, trainable=False)
-        self.learning_rate = tf.compat.v1.train.exponential_decay(learning_rate, self.global_step,
-                                                   100000, 0.96)
+        self.learning_rate = tf.compat.v1.train.exponential_decay(learning_rate, self.global_step, 100000, 0.96)
         self._build_graph()
 
     def _normalize(self):
-        with tf.compat.v1.variable_scope("normalize_inputs") as scope:
-            self.x_norm = tf.contrib.layers.batch_norm(self.x, trainable=False,
-                                epsilon=1e-6,
-                               updates_collections=None, center=False,
-                               scale=False, is_training=self.is_training)
-        with tf.compat.v1.variable_scope("normalize_labels") as scope:
-            self.y_norm = tf.contrib.layers.batch_norm(self.y, trainable=False,
-                                    epsilon=1e-6, updates_collections=None,
-                                  scale=False, is_training=self.is_training)
+        with tf.compat.v1.variable_scope("normalize_inputs", reuse=tf.compat.v1.AUTO_REUSE) as scope:
+            self.x_norm = tf.compat.v1.layers.batch_normalization(
+                self.x, trainable=False, epsilon=1e-6, center=False, scale=False, training=self.is_training)
+        with tf.compat.v1.variable_scope("normalize_labels", reuse=tf.compat.v1.AUTO_REUSE) as scope:
+            self.y_norm = tf.compat.v1.layers.batch_normalization(
+                self.y, trainable=False, epsilon=1e-6, scale=False, training=self.is_training)     
             scope.reuse_variables()
-            self.y_mean = tf.compat.v1.get_variable('BatchNorm/moving_mean')
-            self.y_variance = tf.compat.v1.get_variable('BatchNorm/moving_variance')
-            self.y_beta = tf.compat.v1.get_variable('BatchNorm/beta')
+            self.y_mean = tf.compat.v1.get_variable('batch_normalization/moving_mean')
+            self.y_variance = tf.compat.v1.get_variable('batch_normalization/moving_variance')
+            self.y_beta = tf.compat.v1.get_variable('batch_normalization/beta')
 
     def _inference(self, X):
         for i, k in enumerate(self.filter_sizes):
@@ -82,7 +78,6 @@ class SRCNN:
             err_filled = utils.fill_na(err, 0)
             finite_count = tf.reduce_sum(input_tensor=tf.cast(tf.math.is_finite(err), tf.float32))
             mse = tf.reduce_sum(input_tensor=err_filled) / finite_count
-            return mse
 
     def _optimize(self):
         opt1 = tf.compat.v1.train.AdamOptimizer(self.learning_rate)
@@ -101,7 +96,7 @@ class SRCNN:
                             opt2.apply_gradients(opt2_grads))
 
     def _summaries(self):
-        tf.contrib.layers.summarize_tensors(tf.compat.v1.trainable_variables())
+        # tf.contrib.layers.summarize_tensors(tf.compat.v1.trainable_variables())
         tf.compat.v1.summary.scalar('loss', self.loss)
         tf.compat.v1.summary.scalar('rmse', self.rmse)
 
@@ -116,4 +111,3 @@ class SRCNN:
         self.rmse = tf.sqrt(utils.nanmean(tf.square(self.prediction - self.y)),
                             name='rmse')
         self._summaries()
-
